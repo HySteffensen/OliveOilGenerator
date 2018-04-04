994d237a87fa85ac756f0efc318ca021749bcda0
Hyrum Steffensen <hyrum.steffensen@gmail.com>
2018-04-04 09:38:01 -0700

    created Olive model, instantiated three olives, added those olives to an ArrayList and printed out the names of those olives

--- src/com/example/java/Main.java
+++ src/com/example/java/Main.java
@@ -1,8 +1,23 @@
 package com.example.java;
 
+import com.example.java.model.Olive;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        List<Olive> olives= new ArrayList<>();
+        Olive olive1 = new Olive();
+        olives.add(olive1);
+        Olive olive2 = new Olive();
+        olives.add(olive2);
+        Olive olive3 = new Olive();
+        olives.add(olive3);
+
+        for (Olive o : olives) {
+            System.out.println("o.name = " + o.name);
+        }
     }
 }
